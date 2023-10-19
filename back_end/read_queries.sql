64199bb9774e987ecc84d75c1f88e1009fcef766
camillia <hanaan.amin@yahoo.com>
2023-10-19 16:20:42 -0400

    added read_queries.sql

---
+++ back_end/read_queries.sql
@@ -0,0 +1,18 @@
+/* In relation to https://app.asana.com/0/1205599354289459/1205619395751888/f
+
+Create queries for retrieving:
+    user account info
+    all the events that a user is attending
+    all attendees of an event
+    all the venues available for an event
+    all the caterers available at a venue
+    all the venues available for an event
+    all the events with a specific tag
+    all the events hosted by an organizer 
+    
+*/
+
+/* Get user account info */
+SELECT * FROM User_Account WHERE User_id=1;
+
+/* Get all the events that a user is attending */
