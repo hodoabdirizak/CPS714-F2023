6a0be6123936c7509853a9ab7f0a5490efe3f699
camillia <hanaan.amin@yahoo.com>
2023-11-03 18:34:51 -0400

    updated dbconfig and operations

--- front_end/server.js
+++ front_end/server.js
@@ -1,7 +1,21 @@
+// Run `nodemon server.js` in terminal to run the server
+
 // Initialize Express.js server
 const express       = require('express'),
       dbOperation   = require('../back_end/SQLServerFiles/dbOperation'),
       cors          = require('cors');
+
+
+async function main() {
+  try {
+    const data = await dbOperation();
+    console.dir(data); 
+  } catch (err) {
+    console.log(err);
+  }
+}
+main();
+
 
 /*
 // Define port
@@ -22,11 +36,5 @@ app.get('/quit', function(req,res){
     res.send({result: 'Bye'})
 })
 
-
-// Run nodemon server.js to run the server
 app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
 */
-
-dbOperation.getAccounts().then(res => {
-    console.log(res);
-})
