0bf52e98f57eda97ca4546585a7cf15af0da088b
camillia <hanaan.amin@yahoo.com>
2023-11-03 18:54:06 -0400

    clean up

--- back_end/SQLServerFiles/dbConfig.js
+++ back_end/SQLServerFiles/dbConfig.js
@@ -17,25 +17,5 @@ const config = {
     }
 };
 
-// const config = {
-//     user: 'camillia',
-//     password: '0m@rFa!ls?',
-//     database: 'EventEasy',
-//     server: 'DESKTOP-TR8A4MF',
-//     pool: {
-//         max: 10,
-//         min: 0,
-//         idleTimeoutMillis: 30000
-//     },
-//     options: {
-//         // If you are on Microsoft Azure, you need encryption:
-//         // encrypt: true,
-//         // instancename: 'SQLEXPRESS',
-//         trustedConnection: true,
-//         trustServerCertificate: true
-//     },
-//     port: 1433
-// };
-
 module.exports = config;
 
