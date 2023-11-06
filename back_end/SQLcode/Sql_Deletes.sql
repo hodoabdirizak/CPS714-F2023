b3ebe041ca1f8f9da2a18ca33999976d75454f4d
camillia <hanaan.amin@yahoo.com>
2023-11-05 19:10:07 -0500

    sql code organization

---
+++ back_end/SQLcode/Sql_Deletes.sql
@@ -0,0 +1,49 @@
+
+--INDIVIDUAL DELETION STATEMENTS 
+    
+ /* User_Account deletion
+    Inputs: user
+ */
+DELETE FROM User_Account WHERE User_id = user;
+
+ /* Event_table deletion
+    Inputs: eventid
+ */
+ DELETE FROM Event_table WHERE Event_id = eventid;
+
+/*  Organizer deletion
+    Inputs: User, organizer 
+ */
+DELETE FROM Organizer WHERE User_id = user AND Organizer_id = organizer;
+
+/*  Organizer_events deletion
+    Inputs: eventid, organizer 
+ */
+ DELETE FROM Organizer_events WHERE Event_id = eventid AND Organizer_id = organizer;
+
+ /* Event_Attendees deletion
+    Inputs: user, eventid 
+ */
+DELETE FROM Event_Attendees WHERE Event_id = eventid AND User_id = user;
+
+ /* Venue deletion
+    Inputs: venueid 
+ */
+DELETE FROM Venue WHERE Venue_id = venue;
+
+ /* Caterer deletion
+    Inputs: caterer, eventid 
+ */
+DELETE FROM Caterer WHERE Caterer_id= Caterer AND Event_id = eventid;
+
+ /* Venue_caterer deletion
+    Inputs: caterer, venue 
+ */
+ DELETE FROM Venue_caterer WHERE Venue_id= venue AND caterer_id = caterer;
+
+
+
+
+
+
+
