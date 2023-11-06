0f93bcc7d35c5852fa5990536d6966fff9148922
camillia <hanaan.amin@yahoo.com>
2023-11-05 19:17:07 -0500

    added api routes

--- front_end/server.js
+++ front_end/server.js
@@ -1,52 +1,49 @@
 // Run `nodemon server.js` in terminal to run the server
 
 const express       = require('express'),
-      constructor   = require('../back_end/SQLServerFiles/constructors'),
-      dbOperation   = require('../back_end/SQLServerFiles/dbOperationUserAccount'),
+      controllers   = require('../back_end/controllers'),
       cors          = require('cors');
 
-async function getAccounts() {
-  try {
-    const data = await dbOperation.getAccounts();
-    console.dir(data); 
-  } catch (err) {
-    console.log(err);
-  }
-}
-getAccounts();
-
-// will be determined by input from front-end
-let newAccount = new constructor.User_Account(12, 'ibm@ibm.ca', 'IBM', 6471009000, '', 'organizer');
-
-async function addAccount() {
-  try {
-    await dbOperation.addAccount(newAccount);
-    const data = await dbOperation.getAccounts();
-    console.dir(data); 
-  } catch (err) {
-    console.log(err);
-  }
-}
-addAccount();
-
-/*
 // Define port
 const API_PORT = process.env.PORT || 5000;
 const app = express();
 
+let client, session;
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(cors());
 
-// call for API
-app.get('/api', function(req,res){
-    console.log('Called');
-    res.send({result: 'Message was sent'})
-})
 
-// call for server quit
-app.get('/quit', function(req,res){
-    console.log('Called quit');
-    res.send({result: 'Bye'})
-})
+// ROUTES
+
+// User_Account routes
+// app.get('/api/account/getaccounts', controllers.userAccountController.getAccounts);
+app.post('/api/account/getaccountbyname', controllers.userAccountController.getAccountByName);
+app.post('/api/account/addaccount', controllers.userAccountController.createUserAccount);
+
+
+// Organizer routes
+// app.get('/api/organizer/', controllers.organizerController);
+// app.post('/api/organizer/', controllers.organizerController);
+// app.post('/api/organizer/', controllers.organizerController);
+
+
+// Event routes
+// app.get('/api/event/', controllers.eventController);
+// app.post('/api/event/', controllers.eventController);
+// app.post('/api/event/', controllers.eventController);
+
+
+// Venue routes
+// app.get('/api/venue/', controllers.venueController);
+// app.post('/api/venue/', controllers.venueController);
+// app.post('/api/venue/', controllers.venueController);
+
+
+// Caterer routes
+// app.get('/api/caterer/', controllers.catererController);
+// app.post('/api/caterer/', controllers.catererController);
+// app.post('/api/caterer/', controllers.catererController);
+
 
 app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
-*/
