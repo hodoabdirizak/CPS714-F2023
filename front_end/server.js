bddcd4eb9800bb6adc647d6bfc8b2334295eae06
camillia <hanaan.amin@yahoo.com>
2023-11-22 19:29:00 -0500

    undid commit

--- front_end/server.js
+++ front_end/server.js
@@ -5,7 +5,7 @@ const express       = require('express'),
       cors          = require('cors');
 
 // Define port
-const API_PORT = process.env.PORT || 5000;
+const API_PORT = process.env.PORT || 3000;
 const app = express();
 console.log(app);
 
