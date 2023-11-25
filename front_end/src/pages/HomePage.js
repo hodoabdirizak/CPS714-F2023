d81233771899fb7c0c296e421e16992b6db649c1
4bdulWadood <57127141+4bdulWadood@users.noreply.github.com>
2023-11-24 23:30:53 -0500

    Pop-up Final

--- front_end/src/pages/HomePage.js
+++ front_end/src/pages/HomePage.js
@@ -1,10 +1,7 @@
-// pages/HomePage.js
-
-import React from 'react'
-import { useLocation } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { useEffect, useState } from "react";
 import Navbar from '../components/Navbar';
 import CoverPhoto from '../assets/images/CoverPhoto.jpeg';
 import EventCard from '../components/EventCard';
@@ -21,30 +18,30 @@ export const HomePage = () => {
 
   if (hasID !== 0 && userID === 0) {
       setUserID(hasID);
-  }
+  }  
+
   const getUserID = async () => {
-      console.log("Getting ID for user " + username);
-      const result = await fetch('/api/account/getuseridbyemail', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-              'Accept': 'application/json'
-          },
-          body: JSON.stringify({
-              email: username
-          })
+    console.log("Getting ID for user " + username);
+    const result = await fetch('/api/account/getuseridbyemail', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        email: username
       })
-      var data = await result.json();
-      console.log(data);
-      return data;
-  };
-  
-  if (username !== "" && userID === 0 && hasID === 0) {
-      getUserID().then((res) => {
-          setUserID(res);
-      });
+    });
+    var data = await result.json();
+    console.log(data);
+    return data;
   }
-  console.log(userID);
+
+  if (username !== "" && userID === 0 && hasID === 0) {
+    getUserID().then((res) => {
+        setUserID(res);
+    });
+  }
 
   const [events, setEvents] = useState([
     {
@@ -140,9 +137,38 @@ export const HomePage = () => {
     setShowPopUp(false);
   };
 
+  // State for radio button selection
+  const [rating, setRating] = useState(null);
+
+  // State for text input
+  const [additionalComments, setAdditionalComments] = useState('');
+
+  // State for improvement suggestions
+  const [improvements, setImprovements] = useState('');
+
+  const handleRatingChange = (event) => {
+    setRating(event.target.value);
+  };
+
+  const handleCommentsChange = (event) => {
+    setAdditionalComments(event.target.value);
+  };
+
+  const handleImprovementsChange = (event) => {
+    setImprovements(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    // Add logic to submit feedback to the backend
+    // You can use rating, additionalComments, and improvements states for the feedback data
+    // ...
+    // After submitting, close the pop-up
+    handleClosePopUp();
+  };
+
   return (
     <div className='home-page-container'>
-      <Navbar isLoggedIn={isLoggedIn} username={username} accountType={accountType}/>
+      <Navbar isLoggedIn={isLoggedIn} username={username} accountType={accountType} />
       <div className='background-image'>
         <img src={CoverPhoto} alt='' />
       </div>
@@ -161,6 +187,34 @@ export const HomePage = () => {
           ))}
         </div>
       </div>
+
+      {showPopUp && (
+        <div className='popup-overlay'>
+          <div className='popup'>
+            <div className='popup-content'>
+              <h1>Hi There!</h1>
+              <h2 className="subtitle">You have recently attended the {currentEvent.title} event. We would love to know about your experience at this Event.</h2>
+              <p>How would you rate your overall experience?</p>
+              <div className="radio-buttons">
+                {[1, 2, 3, 4, 5].map((value) => (
+                  <React.Fragment key={value}>
+                    <input type="radio" id={`rating${value}`} name="rating" value={value} checked={rating === `${value}`} onChange={handleRatingChange} />
+                    <label htmlFor={`rating${value}`}>{value}</label>
+                  </React.Fragment>
+                ))}
+              </div>
+              <input type="text" placeholder="Additional comments" value={additionalComments} onChange={handleCommentsChange} />
+              <p>Is there anything we could do to make your next experience with EventEasy better?</p>
+              <input type="text" placeholder="Improvements suggestions" value={improvements} onChange={handleImprovementsChange} />
+              <div className="buttons-container">
+                <button onClick={handleSubmit}>Submit</button>
+                <button onClick={handleClosePopUp}>Remind Me Later</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
+
